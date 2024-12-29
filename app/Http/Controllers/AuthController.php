cc9e4db503d8a326121e11de7f2b89ea3845e0c5
Mohamed Hamed <mohamed.hamed@ntww.com>
2024-12-30 02:36:14 +0300

    Add token name

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
 
         return response()->json([
             'user' => $user,
-            'token' => $user->createToken(),
+            'token' => $user->createToken('invoice-api-token'),
         ]);
     }
 }
